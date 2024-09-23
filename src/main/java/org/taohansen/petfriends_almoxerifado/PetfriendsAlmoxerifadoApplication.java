9bdab661342fc701660f9494ebd7cff301f2db78
Tao Hansen <53713149+taohansens@users.noreply.github.com>
2024-09-22 22:39:36 -0300

    Init project

---
+++ src/main/java/org/taohansen/petfriends_almoxerifado/PetfriendsAlmoxerifadoApplication.java
@@ -0,0 +1,13 @@
+package org.taohansen.petfriends_almoxerifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetfriendsAlmoxerifadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetfriendsAlmoxerifadoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/taohansen/petfriends_almoxerifado/PetfriendsAlmoxerifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taohansen.petfriends_almoxerifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfriendsAlmoxerifadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
